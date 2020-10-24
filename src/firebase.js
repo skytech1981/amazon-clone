eaea9e0b77d19392779e8a2f896c14ec8dbea097
sali itzhaki <sali1943@hotmail.com>
2020-10-24 20:28:12 +0300

    create amazon-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbqvt0pd0ZzIw9gee3XrUlA9hULv1TO1s",
+    authDomain: "clone-9db87.firebaseapp.com",
+    databaseURL: "https://clone-9db87.firebaseio.com",
+    projectId: "clone-9db87",
+    storageBucket: "clone-9db87.appspot.com",
+    messagingSenderId: "730648185452",
+    appId: "1:730648185452:web:4eda75e422f226bb706649",
+    measurementId: "G-7V7CM2PQP5"
+  };
